47708a73067b26c1f1025e11a79102424ef9b3b1
Dkelley7 <d.kelley1835@gmail.com>
2014-10-19 16:22:55 -0400

    changed print

--- Yahtzee/src/yahtzee/Game.java
+++ Yahtzee/src/yahtzee/Game.java
@@ -74,22 +74,72 @@ class Game {
         return this.p2Turn;
     }
     public void populateTable(){
-         ArrayList<Integer> temp = new ArrayList<Integer>();
-            for (int index = 0; index < dice.length; index++)
-            {
-                temp.add(dice[index]);
-            }
-            System.out.println(temp);
+        int numCount[]={0,0,0,0,0,0,0};
+        for(int i=0;i<5;i++){
+            numCount[dice[i]]++;
+        }
         int column= (p1Turn)? 1 : 2;
         //Score Singles
-        for(int i=1;i<=6;i++){
-        if(temp.contains(i)){
-            int count=0;
-            for(int num: temp){
-                if(num==i){count+=i;}
-            }
-        gui.getTable().setValueAt(count, i, column);
-    }else gui.getTable().setValueAt(0, i, column);}
+        for(int index=1;index<7;index++){
+            gui.getTable().setValueAt((index*numCount[index]), index, column);
+        }
+      //3 of  akind
+        {
+        int count=0;
+        boolean set=false;
+	for (int i=1; i<7; i++){
+          if (numCount[i] >= 3)
+              set= true;
+        }if(set){
+		for (int i=1; i<7; i++)
+	  	count = count + (i * numCount[i]);
+                }
+	
+          gui.getTable().setValueAt(count, 9, column);
+        }
+        //4of a kind
+        {
+        int count=0;
+        boolean set=false;
+	for (int i=1; i<7; i++){
+          if (numCount[i] >= 4)
+              set= true;
+        }if(set){
+		for (int i=1; i<7; i++)
+	  	count = count + (i * numCount[i]);
+                }
+	
+          gui.getTable().setValueAt(count, 10, column);
+        }
+        //Full House
+        {
+        int arr[]=numCount;
+        int isSet=0;
+        int score=0;
+        for (int i=1; i<7; i++){
+	  if (arr[i] >= 3)
+		 isSet = i; 
+	}
+	if (isSet > 0){
+		arr[isSet] = arr[isSet] - 3;
+		isSet = 0;
+		for (int i=1; i<7; i++){
+		  if (arr[i] >= 2)
+		 	isSet = i; 
+		}
+		if (isSet > 0)
+                    score=25;
+		
+	}
+        gui.getTable().setValueAt(score, 11, column);  
+         }
+    
+    
+    
+    
+    
+    
     }
+    
 }
-
+	
